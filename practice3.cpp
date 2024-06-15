af7d49d11d852356fde6c223e0fed4cbbd887d6a
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-06-15 10:42:52 +0530

    first commit

---
+++ practice3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, n;
+    cout << "Number:";
+    cin >> n;
+    for (i = -n; i <= n; i++)
+    {
+        cout << i << "\t";
+    }
+
+    return 0;
+}
