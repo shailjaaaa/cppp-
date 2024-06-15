f34045e5b3903510f5edb0f6e78f66a111198718
shailjaaaa <jaganishailja@gmail.com>
2024-06-15 23:31:27 +0530

    change

---
+++ practice10.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    int x=5,y=3;
+    int sum=0;
+    float per=0.0;
+    cout<<"total subject:"<<x;
+    int sub[x];
+
+    
+    return 0;
+}
