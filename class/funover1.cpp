bb26d7bdbefd5275533927773b05ab894ba0229f
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-06-24 11:16:31 +0530

    hjvhj

--- class/funover1.cpp
+++ class/funover1.cpp
@@ -1,25 +1,26 @@
+
 #include<iostream>
 using namespace std;
-class Volume
+class Shape
 {
     public:
-    Volume(int a)
+    void volume(int a)
     {
         //square 
         cout<<"Volume of cube:"<<a*a*a<<"\n";
     }
-    Volume(int a,int b,int c)
+    void volume(int a,int b,int c)
     {
         //rectangle
         cout<<"Volume of cuboid:"<<a*b*c<<"\n";
     }
-    Volume(double a)
+    void volume(double a)
     {
         //Sphere
        float pi=3.14;
         cout<<"Volume of sphere:"<<1.33*pi*a*a*a<<"\n";
     }
-    Volume(int a,int b)
+    void volume(int a,int b)
     {
         //cone
        float pi=3.14;
@@ -28,6 +29,11 @@ class Volume
 };
 int main()
 {
-    Volume obj(10),obj1(10,5,20.),obj2(10.00),obj3(10,20);
+    Shape obj;                                                                                   
+    obj.volume(10);
+    obj.volume(10,5,20);
+    obj.volume(10.00);
+    obj.volume(10,20);
+     
     return 0;
 }
